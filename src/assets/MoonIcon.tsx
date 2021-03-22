67924693e0036cdf00b7666f2b474d39ad512b53
SERKAN <serkanbircan21@yandex.com>
2021-03-22 20:05:03 +0300

    added some icons

---
+++ src/assets/MoonIcon.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default function MoonIcon(props: React.SVGProps<SVGSVGElement>) {
+    return (
+        <svg viewBox="0 0 24 24" fill="currentColor" stroke="currentColor" {...props}><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"></path></svg>
+    );
+}
+
